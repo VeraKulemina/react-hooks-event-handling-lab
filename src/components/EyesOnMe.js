ffe6431ec155acf485518d004f114e5ca30c2426
VeraKulemina <verakulemina@gmail.com>
2022-10-12 14:33:20 -0600

    commit tree ed620766ef31bd70d7da4d34219b50911c5475cd

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function handleFocus(event){
+        console.log("Good!");
+        }
+    function handleBlur(event){
+            console.log("Hey! Eyes on me!");
+            }
+    return(
+        <>
+        <button text="Eyes on me" onFocus={handleFocus} onBlur={handleBlur}>Hi</button>
+        </>
+    )
+}
+export default EyesOnMe;
